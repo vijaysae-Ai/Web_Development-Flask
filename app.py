108f2b32300434f3dd1fc046c077fce1cc297644
vijaysae-Ai <119559709+vijaysae-Ai@users.noreply.github.com>
2023-08-04 09:37:40 +0000

    api route

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,jsonify
 
 app = Flask(__name__)
 
+jobs_data = [
+    {'id': 1, 'title': 'Data Analyst', 'salary': 12333, 'location': 'Bengaluru'},
+    {'id': 2, 'title': 'Data', 'salary': 193039, 'location': 'Bengaluru'},
+    {'id': 3, 'title': 'Data Scientist', 'salary': 123, 'location': 'Bengaluru'},
+    {'id': 4, 'title': 'Data Engineer', 'salary': 10983, 'location': 'Bengaluru'}
+]
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html', jobs=jobs_data)
+
+@app.route('/api/jobs')
+def get_jobs():
+    return jsonify(jobs_data)
+
+
 
 
 if __name__ == '__main__':
